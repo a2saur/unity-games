6c60943b1e7e69b53a7456bac8a3e8276983e924
A2 Saur <a2saur@A2s-MacBook-Pro-3.local>
2024-10-29 13:21:32 -0400

    added 3rd phase and follow attacks to something game

--- something/Assets/Scripts/bossFollowAttack.cs
+++ something/Assets/Scripts/bossFollowAttack.cs
@@ -6,15 +6,53 @@ using UnityEngine;
 
 public class bossFollowAttack : MonoBehaviour
 {
+    public GameObject mc; // doesn't need to be defined
+    public float speed;
+    private float minSpeed = 0.5f;
+    private float timer = 0;
+    private float sizeTimer;
+    private float SIZE_OVERALL = 1f;
+    public Vector3 moveDir;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sizeTimer = SIZE_OVERALL;
+        mc = GameObject.FindWithTag("Player");
+        Vector3 tempDir = new Vector3(
+            mc.transform.position.x-transform.position.x,
+            mc.transform.position.y-transform.position.y,
+            mc.transform.position.z-transform.position.z
+        );
+
+        moveDir = tempDir/tempDir.magnitude;
+
+        speed += (Random.value*(speed-minSpeed))-((speed-minSpeed)/2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (sizeTimer > 0){
+            timer += Time.deltaTime;
+            if (timer >= 3){
+                // fizzle out
+                transform.localScale = new Vector3(sizeTimer/SIZE_OVERALL, sizeTimer/SIZE_OVERALL, sizeTimer/SIZE_OVERALL); // Default
+                sizeTimer -= Time.deltaTime;
+            }
+
+            Vector3 tempDir = new Vector3(
+                mc.transform.position.x-transform.position.x,
+                mc.transform.position.y-transform.position.y,
+                mc.transform.position.z-transform.position.z
+            );
+
+            moveDir = tempDir/tempDir.magnitude;
+
+
+            transform.position = new Vector3(transform.position.x+(moveDir.x*speed*Time.deltaTime), 
+                                            transform.position.y+(moveDir.y*speed*Time.deltaTime),
+                                            transform.position.z+(moveDir.z*speed*Time.deltaTime));
+        }
     }
 }
--- something/Assets/Scripts/voidBoss.cs
+++ something/Assets/Scripts/voidBoss.cs
@@ -7,31 +7,56 @@ public class voidBoss : MonoBehaviour
     public int phase = 1;
     public GameObject basicAttack;
     public GameObject pointAttack;
+    public GameObject followAttack;
 
     private float timer = 0;
+    private float counter = 0;
     private float creationTimer = 0;
     private float creationInterval = 0.25f;
     private int phaseOneMaxTime = 15;
     private int phaseTwoMaxTime = 15;
+    private int phaseThreeMaxTime = 15;
+    private bool secPassed;
+    private bool creation;
+
+    private Animator anim;
+    private Collider2D col;
 
     private List<GameObject> attacks = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        col = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (anim.GetBool("fallen")){
+            col.enabled = true;
+        } else {
+            col.enabled = false;
+        }
+        
         // Timer Handling
         timer += Time.deltaTime;
+        counter += Time.deltaTime;
+        if (timer > 1){
+            secPassed = true;
+            timer = 0;
+        } else {
+            secPassed = false;
+        }
+
         creationTimer += Time.deltaTime;
-        if (phase == 1 && timer > phaseOneMaxTime){ // loop every 20 seconds
-            timer = 0;
-        } if (phase == 2 && timer > phaseTwoMaxTime){ // loop every 20 seconds
-            timer = 0;
+        if (phase == 1 && counter > phaseOneMaxTime){ // loop every 20 seconds
+            counter = 0;
+        } if (phase == 2 && counter > phaseTwoMaxTime){ // loop every 20 seconds
+            counter = 0;
+        } if (phase == 3 && counter > phaseThreeMaxTime){ // loop every 20 seconds
+            counter = 0;
         }
         
         // Scale handling
@@ -41,32 +66,68 @@ public class voidBoss : MonoBehaviour
             transform.localScale = new Vector3(2, 2, 2);
         } if (phase == 3){
             transform.localScale = new Vector3(5, 5, 5);
+            transform.position = new Vector3(transform.position.x, transform.position.y, 1.5f);
         }
 
         // Create attacks
         if (creationTimer > creationInterval){
             creationTimer = 0;
-            if (phase == 1){
-                if ((int)timer < 10){
+            creation = true;
+        } else {
+            creation = false;
+        }
+
+        transform.position = new Vector3 (TimePositionWiggly2(counter+5)+2.5f, TimePositionUpWDownW(counter)+1, transform.position.z);
+        if (phase == 1){
+            if ((int) counter < 10){
+                anim.SetBool("fallen", false);
+                if (creation){
                     // spout basic attacks
                     // create attack
                     GameObject attack = Instantiate(basicAttack, transform.position, Quaternion.identity);
                     attack.GetComponent<bossNormalAttack>().SetDirectionTime(timer, (float) phaseOneMaxTime);
                     attacks.Add(attack);
-                } else {
-                    // wait
                 }
-            } if (phase == 2){
-                if ((int)timer < 10 && (int)timer % 5 == 0){
-                    // spout basic attacks
+            } else {
+                // wait
+                anim.SetBool("fallen", true);
+            }
+        } if (phase == 2){
+            if ((int) counter < 10){
+                anim.SetBool("fallen", false);
+                if ((int) counter % 3 == 0 && creation){
+                    // spout pointing attacks
                     // create attack
                     for (int i = 0; i < 2; i++){
                         GameObject attack = Instantiate(pointAttack, OffsetVector(transform.position, 2), Quaternion.identity);
                         attacks.Add(attack);
-                    }
-                } else {
-                    // wait
                 }
+            }
+            } else {
+                // wait
+                anim.SetBool("fallen", true);
+            }
+        } if (phase == 3){
+            if ((int) counter < 10){
+                anim.SetBool("fallen", false);
+                if ((int) counter % 3 == 0 && secPassed){
+                    // spout follow attacks
+                    // create attack
+                    for (int i = 0; i < 3; i++){
+                        GameObject attack = Instantiate(followAttack, OffsetVector(transform.position, 2), Quaternion.identity);
+                        attacks.Add(attack);
+                    }
+                } else if ((int) counter % 5 == 0 && secPassed){
+                    // spout follow attacks
+                    // create attack
+                    for (int i = 0; i < 5; i++){
+                        GameObject attack = Instantiate(pointAttack, OffsetVector(transform.position, 2), Quaternion.identity);
+                        attacks.Add(attack);
+                    }
+            }
+            } else {
+                // wait, vulnerablec
+                anim.SetBool("fallen", true);
             }
         }
 
@@ -77,8 +138,10 @@ public class voidBoss : MonoBehaviour
             float distance = Vector3.Distance(transform.position, obj.transform.position);
 
             // If the distance is 5 or more, remove and destroy the object
-            if (distance >= 10f)
-            {
+            if (distance >= 10f){
+                attacks.RemoveAt(i); // Remove from the list
+                Destroy(obj);            // Destroy the object in the scene
+            } else if (obj.transform.localScale.x < 0.05){
                 attacks.RemoveAt(i); // Remove from the list
                 Destroy(obj);            // Destroy the object in the scene
             }
@@ -94,5 +157,74 @@ public class voidBoss : MonoBehaviour
         float dy = Random.Range(0, offset*2)-offset;
 
         return new Vector3(initial.x+dx, initial.y+dy, initial.z);
+    }
+
+    // -1 : 1
+    // sin(x) + 0.5cos(x) + 2cos(x + sin(x))
+    float TimePositionSlopeSlant(float x){
+        float s = Mathf.Sin(x);
+        float c = Mathf.Cos(x);
+        float ans = s + c + Mathf.Cos(x + s);
+        return (0.5f*ans);
+    }
+
+    // 1 : 2
+    // sin(cos(cos^2(x))) * 2
+    float TimePositionFlatDips(float x){
+        float ans = Mathf.Sin(Mathf.Cos(Mathf.Cos(x)*Mathf.Cos(x)))*2;
+        return ans;
+    }
+
+    // 0 : 1
+    // sin(x/2)+1
+    float TimePositionSin(float x){
+        float ans = Mathf.Sin(x/2);
+        return ans;
+    }
+
+    // -1 : 3
+    // sin(x)*cos(2x)+cos(x/2)+1
+    float TimePositionWiggly(float x){
+        float ans = Mathf.Sin(x)*Mathf.Cos(2*x)+Mathf.Cos(x/2)+1;
+        return ans;
+    }
+
+    // 1 : 2
+    // (sin(e^x)*cos(x))/x
+    float TimePositionAAABlink(float x){
+        float ans = (Mathf.Sin(Mathf.Exp(x))*Mathf.Cos(x))/x;
+        if (ans > 0){
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
+    // ???
+    // abs | (sin(e^x)*cos(x))/x |
+    float TimePositionAAA(float x){
+        float ans = (Mathf.Sin(Mathf.Exp(x))*Mathf.Cos(x))/x;
+        ans += 1;
+        if (ans > 2){
+            return 2;
+        } else if (ans < -2){
+            return -2;
+        } else {
+            return ans;
+        }
+    }
+
+    // -1 : 1
+    // sin(x + cos(x * 2)) - cos(x / 2)
+    float TimePositionWiggly2(float x){
+        float ans = Mathf.Sin(x + Mathf.Cos(x*2)) - Mathf.Cos(x/2);
+        return ans/2;
+    }
+
+    // -1 : 1
+    // sin(x + cos(2x))
+    float TimePositionUpWDownW(float x){
+        float ans = Mathf.Sin(x + Mathf.Cos(x*2));
+        return ans;
     }
 }
