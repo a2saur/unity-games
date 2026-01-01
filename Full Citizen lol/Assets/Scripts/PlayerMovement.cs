7eba9efa26296bb1880b551a5aa257896e00388c
Alexa Saur <a2saur@mac.mynetworksettings.com>
2025-12-31 21:38:11 -0500

    changed to new input system

--- Full Citizen lol/Assets/Scripts/PlayerMovement.cs
+++ Full Citizen lol/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,18 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Attack attackEffect;
     private float attackDelay;
-
-    private float maxJump = 0.2f;
-    private float curJumpDur = -1;
-
+    
     private int lookingDir = 0;
     private int facingDir = 1;
-    private int movingDir = 0;
+    private float movingDir = 0;
 
     private Rigidbody2D rb;
+
+    public InputActionReference playerControls;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,25 +20,43 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        movingDir = ctx.ReadValue<float>();
+
+        if (movingDir < -0.5){
+            facingDir = -1;
+        } else if (movingDir > 0.5){
+            facingDir = 1;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started) {
+            // started jump
+            rb.AddForce(new Vector3(0, SettingsManager.initialJumpForce, 0));
+        } else if (ctx.canceled) {
+            // ended jump
+            if (rb.linearVelocity.y > 0){
+                SetYVel(rb.linearVelocity.y/2);
+            }
+        }
+    }
+    
+    public void OnAttack(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed && attackDelay <= 0){
+            // attack!
+            attackDelay = SettingsManager.maxAttackDelay;
+            attackEffect.StartAttack(facingDir, lookingDir);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(SettingsManager.attackButton) && attackDelay <= 0){
-            attackDelay = SettingsManager.maxAttackDelay;
-            attackEffect.StartAttack(facingDir, lookingDir);
-        } else {
-            attackDelay -= Time.deltaTime;
-        }
-
-
-        /* --- Jumping --- */
-        if (Input.GetKeyDown(SettingsManager.jumpButton)){
-            rb.AddForce(new Vector3(0, SettingsManager.initialJumpForce, 0));
-        }
-
-        if (Input.GetKeyUp(SettingsManager.jumpButton) && rb.linearVelocity.y > 0){
-            SetYVel(rb.linearVelocity.y/2);
-        }
+        attackDelay -= Time.deltaTime;
 
         // check that the player isn't falling too fast
         if (rb.linearVelocity.y < -SettingsManager.maxFallSpeed){
@@ -51,31 +69,10 @@ public class PlayerMovement : MonoBehaviour
             rb.gravityScale = 1.5f;
         }
 
-        /* --- Moving --- */
-        // Left & right
-        if (Input.GetKeyDown(SettingsManager.rightButton)) {
-            movingDir = 1;
-            facingDir = 1;
-        } if (Input.GetKeyDown(SettingsManager.leftButton)) {
-            movingDir = -1;
-            facingDir = -1;
-        } if (Input.GetKeyUp(SettingsManager.rightButton) && movingDir == 1){
-            movingDir = 0;
-        } if (Input.GetKeyUp(SettingsManager.leftButton) && movingDir == -1){
-            movingDir = 0;
-        }
+        // Move player
         SetXVel(SettingsManager.moveSpeed*movingDir);
 
-        // Up & Down
-        if (Input.GetKeyDown(SettingsManager.upButton)) {
-            lookingDir = 1;
-        } if (Input.GetKeyDown(SettingsManager.downButton)) {
-            lookingDir = -1;
-        } if (Input.GetKeyUp(SettingsManager.upButton) && lookingDir == 1){
-            lookingDir = 0;
-        } if (Input.GetKeyUp(SettingsManager.downButton) && lookingDir == -1){
-            lookingDir = 0;
-        }
+        // TODO: Up & Down arrows
     }
 
     private void SetXVel(float xVel){
